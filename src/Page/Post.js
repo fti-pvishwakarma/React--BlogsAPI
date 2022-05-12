9d4e9e02250539ac79adcd9bc4756a7f3f5edf76
fti-pvishwakarma <pvishwakarma@ftilab.com>
2022-05-12 09:20:07 +0530

    fix issue

--- src/Page/Post.js
+++ src/Page/Post.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-export default function Post({ post }) {
+
+const Post = ({ post }) => {
+  
   return (
-    <div className="container px-5 py-14 mx-auto">
+    <div className="container px-5 py-4 mx-auto">
       <div className="flex flex-wrap -m-4">
-        {post &&
+        {post.length > 0 &&
           post.map((item) => {
             return (
               <div className="p-4 lg:w-1/3" key={item.id}>
-                <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-14 rounded-lg overflow-hidden text-center relative">
-                  <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-2 pb-2 rounded-lg overflow-hidden text-center relative">
+                  <h1 className="title-font sm:text-2xl text-xl text-left font-medium text-gray-900 mb-3">
                     {item.title}
                   </h1>
-                  <p className="leading-relaxed mb-3">{item.body}</p>
+                  <img
+                    src="https://picsum.photos/400/200"
+                    class="max-w-full h-auto"
+                    alt={item.title}
+                  />
+                  <p className="leading-relaxed text-left mb-3">{item.body}</p>
                   <Link
                     to={`${item.id}`}
-                    className="text-indigo-500 inline-flex items-center"
+                    className="text-indigo-500 inline-flex text-right items-center"
                   >
                     Learn More
                     <svg
@@ -38,4 +45,6 @@ export default function Post({ post }) {
       </div>
     </div>
   );
-}
+};
+
+export default Post;
